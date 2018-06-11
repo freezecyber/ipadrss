3eb9164d11cc824d2b8cb32ed2b588064b845f4c
freezecyber <37415540+freezecyber@users.noreply.github.com>
2018-06-11 17:27:08 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ipadress
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                string myHost = System.Net.Dns.GetHostName();
+                string myIP = null;
+
+                for (int i = 0; i <= System.Net.Dns.GetHostEntry(myHost).AddressList.Length - 1; i++)
+                {
+                    if (System.Net.Dns.GetHostEntry(myHost).AddressList[i].IsIPv6LinkLocal == false)
+                    {
+                        myIP = System.Net.Dns.GetHostEntry(myHost).AddressList[i].ToString();
+                    }
+                }
+                Console.WriteLine("--------");
+                Console.WriteLine("local ip : " + myIP);
+                string externalip = new WebClient().DownloadString("http://icanhazip.com");
+                Console.WriteLine("--------");
+                Console.WriteLine("ip adress : " + externalip);
+                Console.ReadLine();
+            }
+            catch { Console.WriteLine("error internet"); }
+        }
+    }
+}
